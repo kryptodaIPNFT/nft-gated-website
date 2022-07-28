d5362207e098c46a9a10e8681f14af62eb71ab7f
Jarrod Watts <jarrodwatts16@gmail.com>
2022-07-28 16:36:49 +1000

    Handle expired auth token

--- pages/index.jsx
+++ pages/index.jsx
@@ -22,12 +22,66 @@ export default function Home() {
 
 // This gets called on every request
 export async function getServerSideProps(context) {
-  // Check to see if they have an authentication cookie
-  const parsedCookies = cookie?.parse(context?.req?.headers?.cookie || "");
-  const authToken = parsedCookies?.["access_token"];
+  try {
+    // Check to see if they have an authentication cookie
+    const parsedCookies = cookie?.parse(context?.req?.headers?.cookie || "");
+    const authToken = parsedCookies?.["access_token"];
 
-  // if there is no auth token, redirect them to the login page
-  if (!authToken) {
+    // if there is no auth token, redirect them to the login page
+    if (!authToken) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    }
+
+    // Ensure we are able to generate an auth token using our private key instantiated SDK
+    const PRIVATE_KEY = process.env.ADMIN_PRIVATE_KEY;
+    if (!PRIVATE_KEY) {
+      throw new Error(
+        "You need to add an ADMIN_PRIVATE_KEY environment variable."
+      );
+    }
+
+    // Instantiate our SDK
+    const sdk = ThirdwebSDK.fromPrivateKey(PRIVATE_KEY, "mumbai");
+
+    // Authenticate token with the SDK
+    const domain = domainName;
+    const address = await sdk.auth.authenticate(domain, authToken);
+
+    // If the auth token is invalid, redirect them to the login page
+    if (!address) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    }
+
+    const hasNft = await checkBalance(sdk, address);
+
+    // If they don't have an NFT, redirect them to the login page
+    if (!hasNft) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    }
+
+    // Finally, return the props
+    return {
+      props: {},
+    };
+  } catch (err) {
+    // If anything goes wrong, redirect them to the login page
+    console.error(err);
+
     return {
       redirect: {
         destination: "/login",
@@ -35,46 +89,4 @@ export async function getServerSideProps(context) {
       },
     };
   }
-
-  // Ensure we are able to generate an auth token using our private key instantiated SDK
-  const PRIVATE_KEY = process.env.ADMIN_PRIVATE_KEY;
-  if (!PRIVATE_KEY) {
-    throw new Error(
-      "You need to add an ADMIN_PRIVATE_KEY environment variable."
-    );
-  }
-
-  // Instantiate our SDK
-  const sdk = ThirdwebSDK.fromPrivateKey(PRIVATE_KEY, "mumbai");
-
-  // Authenticate token with the SDK
-  const domain = domainName;
-  const address = await sdk.auth.authenticate(domain, authToken);
-
-  // If the auth token is invalid, redirect them to the login page
-  if (!address) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
-
-  const hasNft = await checkBalance(sdk, address);
-
-  // If they don't have an NFT, redirect them to the login page
-  if (!hasNft) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
-
-  // Finally, return the props
-  return {
-    props: {},
-  };
 }
